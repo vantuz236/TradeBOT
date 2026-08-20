722a6c7dc557a4608dee10c1388df3a2399f554d
ivan <vk848111230@gmail.com>
2026-08-20 20:52:59 +0700

    prelevel prefilter is already done (maybe)

--- level_detector.py
+++ level_detector.py
@@ -22,14 +22,36 @@ def ext_search(bars):
 def get_prelevel(exts):
     high_levels = [[exts[1][0]]]
     low_levels = [[exts[0][0]]]
+
     for bar in exts[1][1:]:
         for group in high_levels:
             group_price = sum([x.get("high") for x in group])/len(group)
             if group_price * (1 - DIF) < bar.get("high") < group_price * (1+DIF):
                 group.append(bar)
                 break
+        else:
             high_levels.append([bar])
 
+    for bar in exts[0][1:]:
+        for group in low_levels:
+            group_price = sum([x.get("low") for x in group])/len(group)
+            if group_price * (1 - DIF) < bar.get("low") < group_price * (1 + DIF):
+                group.append(bar)
+                break
+        else:
+            low_levels.append([bar])
+
+
+    pre_filter_pre_low_levels = [x for x in low_levels if len(x) >= 3]
+    pre_filter_pre_high_levels = [x for x in high_levels if len(x) >= 3]
+#LOWLEVELS
+    for level in pre_filter_pre_low_levels:
+        level.append(sum([x.get("low") for x in level])/len(level))
+#HIGHLEVELS
+    for level in pre_filter_pre_high_levels:
+        level.append(sum([x.get("high") for x in level])/len(level))
+
+    return [pre_filter_pre_low_levels, pre_filter_pre_high_levels]
 
 
 
